8b7a37131eafc1bd0ec2f69f1925ca3f05042e05
HawiTad <adugtad@gmail.com>
2023-01-20 20:33:20 +0300

    signup page

---
+++ src/pages/Login.page.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div>Login.page</div>
+        /* <div className='w-[25%] flex flex-col items-center justify-center'>
+            <div className='w-[90%] flex flex-col mx-[10px]'>
+              <div className='flex flex-row items-center self-start justify-between w-full mb-3'>
+                <span>Sign in</span>
+                <div>
+                  <span>or</span>
+                  <span className='text-[#5897f9]'> create an account</span>
+                </div>
+              </div>
+              <Button
+                w={'full'}
+                maxW={'lg'}
+                variant={'outline'}
+                leftIcon={<FcGoogle />}
+                className="border-[1px] py-1">
+                <Center w={300}>
+                  <Text>Continue with Google</Text>
+                </Center>
+              </Button>
+            </div>
+            <Box my={8} className='flex flex-col items-center w-[90%]'>
+              <div className='flex flex-row items-center w-full'>
+                <hr className='w-full bg-[#CCC]' />
+                <span className='mx-3'>or</span>
+                <hr className='w-full bg-[#CCC]' />
+              </div>
+              <form className='w-full'>
+    
+                <FormControl className="w-full">
+                  <FormLabel className="text-[#686868]">Email</FormLabel>
+                  <Input size='lg' className='outline-0 border-[1px] border-[#ddd] w-full' />
+                </FormControl>
+                <FormControl className="w-full">
+                  <FormLabel className="text-[#686868]">Password</FormLabel>
+                  <Input type="password" size='md' className='outline-0 border-[1px] border-[#ddd] w-full' />
+                </FormControl>
+                <Box className='flex flex-row items-center justify-between w-'>
+                  <Checkbox size='lg' colorScheme='green' defaultChecked>
+                    Remember me.
+                  </Checkbox>
+                  <Button variantColor="blue" mt={4} className="text-[#fff] bg-[#0080D5] py-[5px] px-3 rounded-[5px]">Sign In</Button>
+                </Box>
+                <Link color="#5897f9" className="text-[14px]">Forgot your password?</Link>
+    
+              </form>
+            </Box>
+          </div> */
+    )
+}
+
+export default Login
---
+++ src/pages/Signup.page.jsx
@@ -0,0 +1,126 @@
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Checkbox,
+    CheckboxGroup,
+    Text,
+    Image,
+    Input,
+    Stack,
+    Box,
+    Link,
+    Button,
+    Center
+} from '@chakra-ui/react'
+import { FcGoogle } from 'react-icons/fc';
+
+function Signup() {
+
+    const SignupSchema = Yup.object().shape({
+        firstName: Yup.string().required('Required'),
+        lastName: Yup.string().required('required'),
+        email: Yup.string().email('Invalid email').required('Required'),
+        password: Yup.string().required("required"),
+    });
+    return (
+
+        <Box display="flex" flexDirection="row" justifyContent="between" w="full" h="100vh">
+            <Box w="25%" display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                <Box w="90%" display="flex" flexDirection="column" mx={10}>
+                    <Text fontWeight="bold" fontSize={23}>Sign up</Text>
+                    <Box display='flex' flexDirection="row">
+                        <Text mr={2}>or </Text>
+                        <Link color='#5897f9' href='#'>sign in to your account</Link>
+                    </Box>
+                </Box>
+                <Formik
+                    initialValues={{
+                        firstName: '',
+                        lastName: '',
+                        email: '',
+                        password: '',
+                    }}
+                    validationSchema={SignupSchema}
+                    onSubmit={(values) => {
+                        alert(JSON.stringify(values, null, 2))
+                    }}>
+                    {({ values, errors, touched, handleChange, handleSubmit }) => (
+
+                        <Box my={8} display="flex" flexDirection="column" alignItems="start" w="90%">
+                            <Box w="100%">
+
+                                <Stack spacing={4}>
+                                    <Input placeholder='First name' size='md' className='border-[1px] px-2 py-1' name='firstName' onChange={handleChange} value={values.firstName} />
+                                    {errors.firstName && touched.firstName ? (<Text fontSize={14} color="#ff0000">{errors.firstName}</Text>) : null}
+                                    <Input placeholder='Last name' size='md' className='border-[1px] px-2 py-1' name='lastName' onChange={handleChange} value={values.lastName} />
+                                    {errors.firstName && touched.lastName ? (<Text fontSize={14} color="#ff0000">{errors.lastName}</Text>) : null}
+                                    <Input placeholder='Email' size='md' className='border-[1px] px-2 py-1' name='email' onChange={handleChange} value={values.email} />
+                                    {errors.firstName && touched.email ? (<Text fontSize={14} color="#ff0000">{errors.email}</Text>) : null}
+                                    <Input placeholder='Password' type="password" size='md' className='border-[1px] px-2 py-1' name='password' onChange={handleChange} value={values.password} />
+                                    {errors.firstName && touched.password ? (<Text fontSize={14} color="#ff0000">{errors.password}</Text>) : null}
+                                </Stack>
+
+                            </Box>
+                            <Text fontSize={13} my={3} color="#bbb">This page is protected by reCAPTCHA, and subject to the Google
+                                <Link color='#5897f9' href='#' ml={1}>
+                                    Privacy Policy
+                                </Link> and
+                                <Link color='#5897f9' href='#' ml={1}>
+                                    Terms of service.
+                                </Link>
+                            </Text>
+                            {/* <Checkbox defaultChecked>I agree to the Dropbox Terms</Checkbox> */}
+                            <Stack spacing={5} mt={5} w="100%">
+                                <Button onClick={handleSubmit} colorScheme='blue' w="90%">Sign up</Button>
+                                <Button
+                                    w="90%"
+                                    maxW={'lg'}
+                                    variant={'outline'}
+                                    leftIcon={<FcGoogle />}
+                                    className="border-[1px] py-1">
+                                    <Center w={300}>
+                                        <Text>Continue with Google</Text>
+                                    </Center>
+                                </Button>
+                            </Stack>
+                        </Box>
+                    )}
+                </Formik>
+            </Box >
+
+            <Box display='flex' flexDirection='column' alignItems="center" justifyContent="center" w='75%' color="#fff" className=' bg-gradient-to-br from-[#000000] to-[#880808]'>
+                <Image src='/assets/logo.png' alt='' />
+                <Text fontSize='50' fontWeight="bold" textAlign="center" className=' leading-normal'>SuperCharge your Product <br /> Listing with AI!</Text>
+                <Box display='flex' flexDirection="column" alignItems="center" mt={50} w="100%">
+                    <Text fontSize='20' fontWeight="bold" mb={20}>Trusted by more than 300 Brands</Text>
+                    <Box display="flex" flexDirection="row" alignItems="center" backgroundColor="#fff" py={3} px={10} w="80%">
+                        <Box mr={10}>
+                            <Image src='/assets/logo1.webp' alt='' width={100} />
+                        </Box>
+                        <Box mr={10}>
+                            <Image src='/assets/logo2.svg' alt='' width={100} />
+                        </Box>
+                        <Box mr={10}>
+                            <Image src='/assets/logo3.svg' alt='' width={100} />
+                        </Box>
+                        <Box mr={10}>
+                            <Image src='/assets/logo4.webp' alt='' width={100} />
+                        </Box>
+                        <Box mr={10}>
+                            <Image src='/assets/logo5.svg' alt='' width={100} mr={10} />
+                        </Box>
+                        <Box mr={10}>
+                            <Image src='/assets/logo6.svg' alt='' width={100} />
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </Box >
+    );
+}
+
+export default Signup;
