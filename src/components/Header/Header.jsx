640f2931f57d94ea0dcd38e7c19fecf77590455a
Adugna Tadesse <adugtad@gmail.com>
2023-09-08 14:45:48 +0300

    latest'

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,179 +1,189 @@
 import {
-    Box,
-    Flex,
-    Text,
-    IconButton,
-    Button,
-    Stack,
-    Collapse,
-    Link,
-    useColorModeValue,
-    useDisclosure,
-    Image,
-} from '@chakra-ui/react';
-import {
-    HamburgerIcon,
-    CloseIcon,
-} from '@chakra-ui/icons';
-
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  Link,
+  useColorModeValue,
+  useDisclosure,
+  Image,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
 const Header = () => {
-    const linkColor = useColorModeValue('white', 'black');
-    const { isOpen, onToggle } = useDisclosure();
+  const linkColor = useColorModeValue("white", "black");
+  const { isOpen, onToggle } = useDisclosure();
 
-    return (
-        <Box>
-            <Flex
-                bg={useColorModeValue('black', 'white')}
-                color={useColorModeValue('white', 'black')}
-                minH={'60px'}
-                py={{ base: 2 }}
-                px={{ base: 4 }}
-                borderBottom={1}
-                borderStyle={'solid'}
-                borderColor={useColorModeValue('gray.200', 'gray.900')}
-                align={'center'}>
-                <Flex
-                    flex={{ md: 'auto' }}
-                    ml={{ base: -2 }}
-                    display={{ base: 'flex', md: 'none' }}>
-                    <IconButton
-                        onClick={onToggle}
-                        icon={
-                            isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
-                        }
-                        variant={'ghost'}
-                        aria-label={'Toggle Navigation'}
-                    />
-                </Flex>
-                <Flex flex={{ base: 1 }} align='center' justify={{ base: 'center', md: 'start' }}>
-                    <Image src='/assets/logo.png' alt='' height={30} display={{ sm: "none", md: "flex" }} />
-                    <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
-                        <Stack direction={'row'} spacing={4}>
-                            {NAV_ITEMS.map((navItem) => (
-                                <Box key={navItem.label}>
-                                    <Link
-                                        p={3}
-                                        href={navItem.href ?? '#'}
-                                        fontSize={'sm'}
-                                        fontWeight={500}
-                                        color={linkColor}
-                                        borderRadius="10px"
-                                        _hover={{
-                                            textDecoration: 'none',
-                                            backgroundColor: '#2C4058'
-
-                                        }}>
-                                        {navItem.label}
-                                    </Link>
-                                </Box>
-                            ))}
-                        </Stack>
-                    </Flex>
-                </Flex>
-                <Stack
-                    flex={{ base: 1, md: 0 }}
-                    justify={'flex-end'}
-                    direction={'row'}
-                    spacing={6}>
-                    <Button
-                        as={'a'}
-                        p={3}
-                        fontSize={'sm'}
-                        fontWeight={400}
-                        variant='link'
-                        color="#fff"
-                        _hover={{
-                            textDecoration: 'none',
-                            backgroundColor: '#2C4058'
-                        }}
-                        href={'#'}>
-                        2 Credits
-                    </Button>
-                    <Button
-
-                        fontSize={'sm'}
-                        fontWeight={600}
-                        color={'white'}
-                        bg="#3182CE"
-                        href={'#'}>
-                        Upgrade
-                    </Button>
-                </Stack>
-            </Flex>
-            <Collapse in={isOpen} animateOpacity>
-                <MobileNav />
-            </Collapse>
-        </Box>
-    );
+  return (
+    <Box>
+      <Flex
+        bg={useColorModeValue("black", "white")}
+        color={useColorModeValue("white", "black")}
+        minH={"60px"}
+        py={{ base: 2 }}
+        px={{ base: 4 }}
+        borderBottom={1}
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.900")}
+        align={"center"}
+      >
+        <Flex
+          flex={{ md: "auto" }}
+          ml={{ base: -2 }}
+          display={{ base: "flex", md: "none" }}
+        >
+          <IconButton
+            onClick={onToggle}
+            icon={
+              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+            }
+            variant={"ghost"}
+            aria-label={"Toggle Navigation"}
+          />
+        </Flex>
+        <Flex
+          flex={{ base: 1 }}
+          align="center"
+          justify={{ base: "center", md: "start" }}
+        >
+          <Link href="/">
+            <Image
+              src="/assets/logo.png"
+              alt=""
+              height={30}
+              display={{ sm: "none", md: "flex" }}
+            />
+          </Link>
+          <Flex display={{ base: "none", md: "flex" }} ml={10}>
+            <Stack direction={"row"} spacing={4}>
+              {NAV_ITEMS.map((navItem) => (
+                <Box key={navItem.label}>
+                  <Link
+                    p={3}
+                    href={navItem.page ?? "#"}
+                    fontSize={"sm"}
+                    fontWeight={500}
+                    color={linkColor}
+                    borderRadius="10px"
+                    _hover={{
+                      textDecoration: "none",
+                      backgroundColor: "#2C4058",
+                    }}
+                  >
+                    {navItem.label}
+                  </Link>
+                </Box>
+              ))}
+            </Stack>
+          </Flex>
+        </Flex>
+        <Stack
+          flex={{ base: 1, md: 0 }}
+          justify={"flex-end"}
+          direction={"row"}
+          spacing={6}
+        >
+          {/* <Button
+            as={"a"}
+            p={3}
+            fontSize={"sm"}
+            fontWeight={400}
+            variant="link"
+            color="#fff"
+            _hover={{
+              textDecoration: "none",
+              backgroundColor: "#2C4058",
+            }}
+            href={"#"}
+          >
+            2 Credits
+          </Button> */}
+          <Button fontSize={"sm"} fontWeight={600} color={"white"} bg="#3182CE">
+            <Link href="/upgrade">Upgrade</Link>
+          </Button>
+        </Stack>
+      </Flex>
+      <Collapse in={isOpen} animateOpacity>
+        <MobileNav />
+      </Collapse>
+    </Box>
+  );
 };
 
 export default Header;
 
 const MobileNav = () => {
-    return (
-        <Stack
-            bg={useColorModeValue('white', 'gray.800')}
-            p={4}
-            display={{ md: 'none' }}>
-            {NAV_ITEMS.map((navItem) => (
-                <MobileNavItem key={navItem.label} {...navItem} />
-            ))}
-        </Stack>
-    );
+  return (
+    <Stack
+      bg={useColorModeValue("white", "gray.800")}
+      p={4}
+      display={{ md: "none" }}
+    >
+      {NAV_ITEMS.map((navItem) => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  );
 };
 
 const MobileNavItem = ({ label, children, href }) => {
-    const { isOpen, onToggle } = useDisclosure();
+  const { isOpen, onToggle } = useDisclosure();
 
-    return (
-        <Stack spacing={4} onClick={children && onToggle}>
-            <Flex
-                py={2}
-                as={Link}
-                href={href ?? '#'}
-                justify={'center'}
-                align={'center'}
-                _hover={{
-                    textDecoration: 'none',
-                }}>
-                <Text
-                    fontWeight={600}
-                    color={useColorModeValue('gray.600', 'gray.200')}>
-                    {label}
-                </Text>
-            </Flex>
+  return (
+    <Stack spacing={4} onClick={children && onToggle}>
+      <Flex
+        py={2}
+        as={Link}
+        href={href ?? "#"}
+        justify={"center"}
+        align={"center"}
+        _hover={{
+          textDecoration: "none",
+        }}
+      >
+        <Text
+          fontWeight={600}
+          color={useColorModeValue("gray.600", "gray.200")}
+        >
+          {label}
+        </Text>
+      </Flex>
 
-            <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
-                <Stack
-                    mt={2}
-                    pl={4}
-                    borderLeft={1}
-                    borderStyle={'solid'}
-                    borderColor={useColorModeValue('gray.200', 'gray.700')}
-                    align={'start'}>
-                    {children &&
-                        children.map((child) => (
-                            <Link key={child.label} py={2} href={child.href}>
-                                {child.label}
-                            </Link>
-                        ))}
-                </Stack>
-            </Collapse>
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: "0!important" }}>
+        <Stack
+          mt={2}
+          pl={4}
+          borderLeft={1}
+          borderStyle={"solid"}
+          borderColor={useColorModeValue("gray.200", "gray.700")}
+          align={"start"}
+        >
+          {children &&
+            children.map((child) => (
+              <Link key={child.label} py={2} href={child.href}>
+                {child.label}
+              </Link>
+            ))}
         </Stack>
-    );
+      </Collapse>
+    </Stack>
+  );
 };
 
-
 const NAV_ITEMS = [
-    {
-        label: 'New',
-
-    },
-    {
-        label: 'Listings',
-    },
-    {
-        label: 'Account',
-    },
+  {
+    label: "New",
+    page: "",
+  },
+  {
+    label: "Listings",
+    page: "/listing",
+  },
+  {
+    label: "Account",
+    page: "/account",
+  },
 ];
--- src/pages/Login.page.jsx
+++ src/pages/Login.page.jsx
@@ -1,110 +1,186 @@
 import {
-    FormControl,
-    FormLabel,
-    FormErrorMessage,
-    FormHelperText,
-    Checkbox,
-    CheckboxGroup,
-    Text,
-    Image,
-    Input,
-    Stack,
-    Box,
-    Link,
-    Button,
-    Center,
-    Heading,
-    Flex,
-    Spinner
-} from '@chakra-ui/react'
-import { Formik } from 'formik';
-import { FcGoogle } from 'react-icons/fc';
-import LandingPage from '../components/LandingPage/LandingPage';
-import * as Yup from 'yup';
-import { useState } from 'react';
-
-
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Checkbox,
+  CheckboxGroup,
+  Text,
+  Image,
+  Input,
+  Stack,
+  Box,
+  Link,
+  Button,
+  Center,
+  Heading,
+  Flex,
+  Spinner,
+} from "@chakra-ui/react";
+import { Formik } from "formik";
+import { FcGoogle } from "react-icons/fc";
+import LandingPage from "../components/LandingPage/LandingPage";
+import * as Yup from "yup";
+import { useState } from "react";
 
 const Login = () => {
-    const [Overlay, setOverlay] = useState(false);
-    const LoginSchema = Yup.object().shape({
-        email: Yup.string().email('Invalid email').required('Required'),
-        password: Yup.string().required("required"),
-    });
-    return (
-        <Box display="flex" flexDirection={{ sm: "column-reverse", md: "row" }} alignItems={{ sm: "center", md: "normal" }} justifyContent={{ sm: "start", md: "between" }} w="full" minH="100vh">
-            <Box width={{ sm: "90%", md: "30%" }} display="flex" flexDirection="column" alignItems="center" justifyContent="center" py={5} position="relative">
-                <Flex display={Overlay ? "flex" : "none"} alignItems="center" justifyContent="center" w="100%" h="100%" bgColor="#5A05052F" position="absolute" top="0" zIndex={2}>
-                    <Spinner
-                        thickness='4px'
-                        speed='0.65s'
-                        emptyColor='gray.200'
-                        color='#5A0505'
-                        size='xl'
-                    />
-                </Flex>
-                <Box display="flex" flexDirection="column" mx={10} w="90%">
-                    <Box display="flex" flexDirection="row" alignItems="center" justifyContent="space-between" w="100%" mb={3} className='self-start'>
-                        <Heading mb={3}>Sign in</Heading>
-                        <Box display="flex" flexDirection="row" alignItems="center">
-                            <Text fontSize="14px" mr={1}>or</Text>
-                            <Link color="#5897f9" href='#' fontSize="14px"> create an account</Link>
-                        </Box>
-                    </Box>
-                    <Button
-                        w="100%"
-                        variant={'outline'}
-                        leftIcon={<FcGoogle />}
-                        className="border-[1px] py-1">
-                        <Center w="100%">
-                            <Text>Continue with Google</Text>
-                        </Center>
-                    </Button>
-                </Box>
-                <Box display="flex" flexDirection="column" alignItems="center" w="90%" my={8} >
-                    <Box display="flex" flexDirection="row" alignItems="center" w="100%">
-                        <hr className='w-full' />
-                        <Text mx={3} className='mx-3'>or</Text>
-                        <hr className='w-full' />
-                    </Box>
-                    <Formik
-                        initialValues={{
-                            email: '',
-                            password: '',
-                        }}
-                        validationSchema={LoginSchema}
-                        onSubmit={(values) => {
-                            setOverlay(true)
-                            alert(JSON.stringify(values, null, 2))
-                        }}>
-                        {({ values, errors, touched, handleChange, handleSubmit }) => (
-
-                            <Box w="100%" spacing={3}>
-
-                                <FormControl w="100%">
-                                    <FormLabel color="#686868">Email</FormLabel>
-                                    <Input size='md' w="100%" name='email' className='outline-0 border-[1px] border-[#ddd' onChange={handleChange} value={values.email} />
-                                    {errors.email && touched.email ? (<Text fontSize={14} color="#ff0000">{errors.email}</Text>) : null}
-                                </FormControl>
-                                <FormControl w="100%">
-                                    <FormLabel color="#686868">Password</FormLabel>
-                                    <Input type="password" size='md' w="100%" name='password' className='outline-0 border-[1px] border-[#ddd]' onChange={handleChange} value={values.password} />
-                                    {errors.password && touched.password ? (<Text fontSize={14} color="#ff0000">{errors.password}</Text>) : null}
-                                </FormControl>
-                                <Box display="flex" flexDirection="row" alignItems="center" justifyContent="space-between" mt={4} w="100%">
-                                    <Checkbox defaultChecked>Remember me</Checkbox>
-                                    <Button onClick={handleSubmit} colorScheme='blue'>Sign In</Button>
-                                </Box>
-                                <Link color="#5897f9" href='#' fontSize="14px">Forgot your password?</Link>
-
-                            </Box>
-                        )}
-                    </Formik>
-                </Box>
+  const [Overlay, setOverlay] = useState(false);
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required("Required"),
+    password: Yup.string().required("required"),
+  });
+  return (
+    <Box
+      display="flex"
+      flexDirection={{ sm: "column-reverse", md: "row" }}
+      alignItems={{ sm: "center", md: "normal" }}
+      justifyContent={{ sm: "start", md: "between" }}
+      w="full"
+      minH="100vh"
+    >
+      <Box
+        width={{ sm: "90%", md: "30%" }}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        py={5}
+        position="relative"
+      >
+        <Flex
+          display={Overlay ? "flex" : "none"}
+          alignItems="center"
+          justifyContent="center"
+          w="100%"
+          h="100%"
+          bgColor="#5A05052F"
+          position="absolute"
+          top="0"
+          zIndex={2}
+        >
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="#5A0505"
+            size="xl"
+          />
+        </Flex>
+        <Box display="flex" flexDirection="column" mx={10} w="90%">
+          <Box
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+            w="100%"
+            mb={3}
+            className="self-start"
+          >
+            <Heading mb={3}>Sign in</Heading>
+            <Box display="flex" flexDirection="row" alignItems="center">
+              <Text fontSize="14px" mr={1}>
+                or
+              </Text>
+              <Link color="#5897f9" href="/signup" fontSize="14px">
+                create an account
+              </Link>
             </Box>
-            <LandingPage />
+          </Box>
+          {/* <Button
+            w="100%"
+            variant={"outline"}
+            leftIcon={<FcGoogle />}
+            className="border-[1px] py-1"
+          >
+            <Center w="100%">
+              <Text>Continue with Google</Text>
+            </Center>
+          </Button> */}
         </Box>
-    )
-}
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          w="90%"
+          my={8}
+        >
+          <Box display="flex" flexDirection="row" alignItems="center" w="100%">
+            <hr className="w-full" />
+            <Text mx={3} className="mx-3">
+              or
+            </Text>
+            <hr className="w-full" />
+          </Box>
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            validationSchema={LoginSchema}
+            onSubmit={(values) => {
+              setOverlay(true);
+              alert(JSON.stringify(values, null, 2));
+            }}
+          >
+            {({ values, errors, touched, handleChange, handleSubmit }) => (
+              <Box w="100%" spacing={3}>
+                <FormControl w="100%">
+                  <FormLabel color="#686868">Email</FormLabel>
+                  <Input
+                    size="md"
+                    w="100%"
+                    name="email"
+                    className="outline-0 border-[1px] border-[#ddd"
+                    onChange={handleChange}
+                    value={values.email}
+                  />
+                  {errors.email && touched.email ? (
+                    <Text fontSize={14} color="#ff0000">
+                      {errors.email}
+                    </Text>
+                  ) : null}
+                </FormControl>
+                <FormControl w="100%">
+                  <FormLabel color="#686868">Password</FormLabel>
+                  <Input
+                    type="password"
+                    size="md"
+                    w="100%"
+                    name="password"
+                    className="outline-0 border-[1px] border-[#ddd]"
+                    onChange={handleChange}
+                    value={values.password}
+                  />
+                  {errors.password && touched.password ? (
+                    <Text fontSize={14} color="#ff0000">
+                      {errors.password}
+                    </Text>
+                  ) : null}
+                </FormControl>
+                <Box
+                  display="flex"
+                  flexDirection="row"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  mt={4}
+                  w="100%"
+                >
+                  <Checkbox defaultChecked>Remember me</Checkbox>
+                  <Button onClick={handleSubmit} colorScheme="blue">
+                    Sign In
+                  </Button>
+                </Box>
+                <Link color="#5897f9" href="#" fontSize="14px">
+                  Forgot your password?
+                </Link>
+              </Box>
+            )}
+          </Formik>
+        </Box>
+      </Box>
+      <LandingPage />
+    </Box>
+  );
+};
 
-export default Login
+export default Login;
